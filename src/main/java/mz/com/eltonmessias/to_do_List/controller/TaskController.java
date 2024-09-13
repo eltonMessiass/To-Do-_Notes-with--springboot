61aee2871a061ccfb4146422bde8f74f923a4234
eltonMessiass <eltonmessias10@gmail.com>
2024-09-13 22:03:32 +0200

    list and create tasks

--- src/main/java/mz/com/eltonmessias/to_do_List/controller/TaskController.java
+++ src/main/java/mz/com/eltonmessias/to_do_List/controller/TaskController.java
@@ -1,4 +1,34 @@
 package mz.com.eltonmessias.to_do_List.controller;
 
+import mz.com.eltonmessias.to_do_List.dto.TaskDTO;
+import mz.com.eltonmessias.to_do_List.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping("/tasks")
+    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
+        TaskDTO createdTask = taskService.createTask(taskDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
+    }
+
+    @GetMapping("/tasks")
+    public ResponseEntity<List<TaskDTO>> getAllTasks() {
+        return ResponseEntity.status(HttpStatus.OK).body(taskService.getAllTasks());
+    }
+
+
+
 }
--- src/main/java/mz/com/eltonmessias/to_do_List/service/TaskService.java
+++ src/main/java/mz/com/eltonmessias/to_do_List/service/TaskService.java
@@ -1,8 +1,45 @@
 package mz.com.eltonmessias.to_do_List.service;
 
 
+import mz.com.eltonmessias.to_do_List.dto.TaskDTO;
+import mz.com.eltonmessias.to_do_List.model.TaskModel;
+import mz.com.eltonmessias.to_do_List.repository.TaskRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    private TaskDTO convertToDTO(TaskModel taskModel) {
+        TaskDTO taskDTO = new TaskDTO(taskModel.getId(), taskModel.getName(), taskModel.getDescription(), taskModel.isCompleted());
+//        BeanUtils.copyProperties(taskModel, taskDTO);
+        return taskDTO;
+    }
+
+    private TaskModel convertToModel(TaskDTO taskDTO) {
+        TaskModel taskModel = new TaskModel();
+        BeanUtils.copyProperties(taskDTO, taskModel);
+        return taskModel;
+    }
+
+    public TaskDTO createTask(TaskDTO taskDTO) {
+        TaskModel taskModel = new TaskModel();
+        BeanUtils.copyProperties(taskDTO, taskModel);
+        TaskModel savedTask = taskRepository.save(taskModel);
+        BeanUtils.copyProperties(savedTask, taskDTO);
+        return taskDTO;
+    }
+    
+    public List<TaskDTO> getAllTasks(){
+        return taskRepository.findAll().stream().map(this::convertToDTO).collect(Collectors.toList());
+    }
 }
